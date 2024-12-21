6344a827e26e3d642a10b6b08cddd92dedc4923a
pk.the.dev <pk.the.dev@gmail.com>
2024-12-21 17:14:44 +0530

    assets issue fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/chess-solver/",
-  plugins: [react()],
+  base: '/chess-solver/',
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/assets/pieces', // Source folder
+          dest: 'assets/pieces', // Destination folder in dist
+        },
+      ],
+    }),
+  ],
 })
